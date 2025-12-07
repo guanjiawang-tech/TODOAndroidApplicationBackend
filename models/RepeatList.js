127a55dd6433aab7685670079e48a39070cd317a
guanjiawang <15041059652@163.com>
2025-12-07 15:47:41 +0800

    重构repeat_List数据表

--- models/RepeatList.js
+++ models/RepeatList.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const RepeatListSchema = new mongoose.Schema({
   todoId: { type: String, required: true },
+  userId: String,
   date: { type: [String], default: [] }, // 存日期数组
 });
 
